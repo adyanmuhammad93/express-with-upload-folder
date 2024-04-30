d3ca7983e75d05129dd7a590650e96834688c78c
Adyan Muhammad <adyanmuhammad932gmail.com>
2024-04-30 14:24:19 +0700

    add mysql connection

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,9 +1,28 @@
-// YOUR_BASE_DIRECTORY/netlify/functions/api.ts
 import express, { Router } from "express";
 import serverless from "serverless-http";
+import mysql from "mysql2/promise";
 
 const api = express();
 const router = Router();
+
+// Create a connection to the database
+const db = mysql.createPool({
+  host: "pdsi.dev.grid.co.id",
+  user: "development_pdsi",
+  password: "2GTa2eJSeC",
+  database: "development_pdsi",
+});
+
+async function testConnection() {
+  try {
+    await db.getConnection();
+    console.log("Successfully connected to the database!");
+  } catch (error) {
+    console.error("An error occurred while connecting to the database:", error);
+  }
+}
+
+testConnection();
 
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
