4542e39f590ea16ffcd1be07f2155ae910060e38
Adyan Muhammad <adyanmuhammad932gmail.com>
2024-05-03 20:00:09 +0700

    update

--- netlify/functions/routes/laporanTahunanRoutes.js
+++ netlify/functions/routes/laporanTahunanRoutes.js
@@ -7,6 +7,7 @@ import {
   deleteLaporanTahunan,
 } from "../services/laporanTahunanServices.js";
 import multer from "multer";
+import cors from "cors";
 
 // Multer configuration for file uploads
 const storage = multer.memoryStorage();
@@ -132,5 +133,7 @@ laporanTahunanRoutes.delete("/laporan-tahunan/:id", async (req, res) => {
     });
   }
 });
+
+laporanTahunanRoutes.use(cors());
 
 export default laporanTahunanRoutes;
