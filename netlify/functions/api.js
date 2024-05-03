1d0440cc158a2e6378f959111ea6211e05c28c78
Adyan Muhammad <adyanmuhammad932gmail.com>
2024-05-03 20:06:55 +0700

    update

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -25,6 +25,13 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 export const handler = serverless(app);
 
 // app.use((err, res) => {
