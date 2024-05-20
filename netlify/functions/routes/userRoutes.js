344166b21ca2ae5b8d85854f3b1a3953f7d629e6
Adyan Muhammad <adyanmuhammad932gmail.com>
2024-05-21 06:06:40 +0700

    add user

--- netlify/functions/routes/userRoutes.js
+++ netlify/functions/routes/userRoutes.js
@@ -1,11 +1,18 @@
 import { Router } from "express";
-import { authenticate, register } from "../services/userServices.js";
+import {
+  authenticate,
+  deleteUser,
+  getAllUsers,
+  getUserById,
+  register,
+  updateUser,
+} from "../services/userServices.js";
 
 const userRoutes = Router();
 
 userRoutes.post("/register", async (req, res) => {
-  const { username, password, role } = req.body;
-  const result = await register(username, password, role);
+  const { username, password, role, privileges } = req.body;
+  const result = await register(username, password, role, privileges);
 
   res.json(result);
 });
@@ -24,6 +31,42 @@ userRoutes.post("/logout", (req, res) => {
   // In a stateless authentication mechanism like JWT, logging out can be handled on the client side
   // When a user logs out, you simply need to destroy the token on the client side
   res.json({ message: "Logged out successfully" });
+});
+
+userRoutes.get("/users", async (req, res) => {
+  try {
+    const users = await getAllUsers();
+    res.json(users);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+userRoutes.get("/users/:id", async (req, res) => {
+  try {
+    const user = await getUserById(req.params.id);
+    res.json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+userRoutes.put("/users/:id", async (req, res) => {
+  try {
+    const result = await updateUser(req.params.id, req.body);
+    res.json(result);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+userRoutes.delete("/users/:id", async (req, res) => {
+  try {
+    const result = await deleteUser(req.params.id);
+    res.json(result);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 export default userRoutes;
--- netlify/functions/services/userServices.js
+++ netlify/functions/services/userServices.js
@@ -12,9 +12,7 @@ const authenticate = async (username, password) => {
 
     if (user) {
       if (user.password === password) {
-        const token = jwt.sign({ sub: user.id, role: user.role }, SECRET_KEY, {
-          expiresIn: "1h",
-        });
+        const token = jwt.sign({ sub: user.id, role: user.role }, SECRET_KEY);
         return { ...user, token };
       } else {
         throw new Error("Password is incorrect");
@@ -28,11 +26,23 @@ const authenticate = async (username, password) => {
   }
 };
 
-const register = async (username, password, role) => {
+const register = async (username, password, role, privileges) => {
+  console.log(privileges);
   try {
+    const defaultPrivileges = {
+      berita: false,
+      laporanTahunan: false,
+      majalah: false,
+      penghargaan: false,
+      slider: false,
+    };
+
+    // If privileges are not provided, use the default privileges
+    const finalPrivileges = privileges || defaultPrivileges;
+
     const [result] = await db.execute(
-      "INSERT INTO users (username, password, role) VALUES (?, ?, ?)",
-      [username, password, role]
+      "INSERT INTO users (username, password, role, privileges) VALUES (?, ?, ?, ?)",
+      [username, password, role, JSON.stringify(finalPrivileges)]
     );
 
     if (result.affectedRows > 0) {
@@ -45,4 +55,72 @@ const register = async (username, password, role) => {
   }
 };
 
-export { authenticate, register };
+const getAllUsers = async () => {
+  try {
+    const [users] = await db.execute("SELECT * FROM users");
+
+    // Parse the privileges string for each user
+    users.forEach((user) => {
+      try {
+        // Check if user.privileges is a valid JSON string
+        JSON.parse(user.privileges);
+        user.privileges = JSON.parse(user.privileges);
+      } catch (error) {
+        console.error(`Error parsing privileges for user ${user.id}:`, error);
+      }
+    });
+
+    return users;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+const getUserById = async (userId) => {
+  try {
+    const [users] = await db.execute("SELECT * FROM users WHERE id = ?", [
+      userId,
+    ]);
+    return users[0];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+const updateUser = async (userId, updates) => {
+  try {
+    const fields = Object.keys(updates)
+      .map((key) => `${key} = ?`)
+      .join(", ");
+    const values = Object.values(updates);
+    await db.execute(`UPDATE users SET ${fields} WHERE id = ?`, [
+      ...values,
+      userId,
+    ]);
+    return { message: "User updated successfully" };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+const deleteUser = async (userId) => {
+  try {
+    await db.execute("DELETE FROM users WHERE id = ?", [userId]);
+    return { message: "User deleted successfully" };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export {
+  authenticate,
+  register,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+};
